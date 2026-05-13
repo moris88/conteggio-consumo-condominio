8ca7a5c16fa7162034d18134124070092d0fc3f2
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2026-05-13 14:55:59 +0200

    refactor: aggiungere mock per gestire il click su link di download in JSDOM

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+
+// Mock per evitare "Error: Not implemented: navigation (except hash changes)" in JSDOM
+// quando si usa a.click() per il download di file
+const originalClick = HTMLAnchorElement.prototype.click
+HTMLAnchorElement.prototype.click = function () {
+	if (this.href.startsWith('blob:') || this.download) {
+		return
+	}
+	originalClick.call(this)
+}
